ac772ff93f6ae1cb62b7c9db38dd9edfb3f67912
Joshjwood <joshjwood91@gmail.com>
2022-02-19 02:03:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import smtplib
+import time
+from weather_alerts import tomorrow_weather, today_weather, get_weather_data
+from news_reports import bbc_today, reuters_today, guardian_today, yesterday_top_3
+from birthday_alert import birthday_alert
+from privates import *
+
+my_email = MY_EMAIL
+password = PASSWORD
+my_main_email = MY_MAIN_EMAIL
+
+weather_data = get_weather_data()
+news_block = bbc_today() + reuters_today() + guardian_today() + yesterday_top_3()
+
+#########Sending#########
+message = today_weather(weather_data) + tomorrow_weather(weather_data) + "\n\n" + birthday_alert() + "\n\n" + news_block + f"\n \nThis email has been brought to you by Josh Wood's automation services, making janky shit work since several months ago.\n \n JWA: Fail upwards"
+
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs=my_main_email, msg=message.encode("utf-8"))
+time.sleep(10)
+connection.close()
